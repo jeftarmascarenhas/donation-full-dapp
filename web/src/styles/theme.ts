e2fb11ba41c9b4cb573534ca8f8cfad13f3bced3
Jeff Mascarenhas <jeftarmascarenhas@hotmail.com>
2022-12-12 21:03:27 -0300

    style: theme dark color updated

--- web/src/styles/theme.ts
+++ web/src/styles/theme.ts
@@ -2,12 +2,11 @@ import { extendTheme, Theme } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
 
 const light = "#fff";
-const dark =
-  "linear-gradient(180deg, rgba(0, 0, 0, 0) 43.12%, rgba(0, 0, 0, 0.35) 57.08%), linear-gradient(180deg, rgba(25, 28, 31, 0.52) 40.21%, rgba(25, 28, 31, 0.52) 100%), radial-gradient(100% 197.14% at 100% 15.88%, #111F38 11.4%, #0F1318 33.05%, #0F1318 52%, #1C0731 77.8%)";
+const dark = "#222";
 
 const theme: Theme = extendTheme({
   config: {
-    initialColorMode: "dark",
+    initialColorMode: "light",
     useSystemColorMode: false,
   } as Theme["config"],
   styles: {
